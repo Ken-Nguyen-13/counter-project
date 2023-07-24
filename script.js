c4b4200d62b512a42e12b1baa05923e09642a7f8
Ken-Nguyen-13 <ken.ak.nguyen@gmail.com>
2023-07-24 19:08:44 -0400

    styled main with colors and added a confirm message to reset btn

--- script.js
+++ script.js
@@ -4,15 +4,36 @@ const increaseBtn = document.getElementById("increase");
 const counter = document.getElementById("counter");
 let counterValue = Number(counter.textContent);
 
+function defineColor() {
+  if (counterValue > 0) {
+    counter.style.color = "rgb(0, 255, 0)";
+  } else if (counterValue < 0) {
+    counter.style.color = "rgb(255, 0, 0)";
+  } else {
+    counter.style.color = "rgba(0, 0, 0, 0)";
+  }
+}
+function confirmReset() {
+  if (confirm("Are you sure you want to reset counter?") === true) {
+    return true;
+  } else {
+    return false;
+  }
+}
 decreaseBtn.addEventListener("click", function () {
   counterValue--;
   counter.textContent = counterValue;
+  defineColor();
 });
 resetBtn.addEventListener("click", function () {
-  counter.textContent = 0;
-  counterValue = 0;
+  if (counterValue !== 0 && confirmReset()) {
+    counter.textContent = 0;
+    counterValue = 0;
+    defineColor();
+  }
 });
 increaseBtn.addEventListener("click", function () {
   counterValue++;
   counter.textContent = counterValue;
+  defineColor();
 });
